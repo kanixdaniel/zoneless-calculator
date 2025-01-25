4b3b21237f9b43c03da1ffbaa8403baa009094ab
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2025-01-25 17:39:31 -0600

    Feat: componente contenedor de la vista de calculadora

---
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorButtonComponent } from "../calculator-button/calculator-button.component";
+
+@Component({
+    selector: 'calculator',
+    imports: [CalculatorButtonComponent],
+    templateUrl: './calculator.component.html',
+    /* styles: `
+    .is-command {
+        @apply  bg-indigo-700/10;
+    }
+    `, */
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CalculatorComponent { }
