bdf896be25a1662814fd61d842ec6b717ba0da5d
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2025-01-25 22:20:17 -0600

    Feat: servicio para generar los textos de la calculadora y realizar las operaciones

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,5 +1,15 @@
-import { ChangeDetectionStrategy, Component, viewChildren } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, viewChildren } from '@angular/core';
 import { CalculatorButtonComponent } from "../calculator-button/calculator-button.component";
+import { CalculatorService } from '@/calculator/services/calculator.service';
+
+const keyEquivalents: Record<string, string> = {
+    Escape: 'C',
+    Clear: 'C',
+    c: 'C',
+    '*': '⨉',
+    '/': '÷',
+    Enter: '='
+}
 
 @Component({
     selector: 'calculator',
@@ -16,23 +26,20 @@ import { CalculatorButtonComponent } from "../calculator-button/calculator-butto
     }
 })
 export class CalculatorComponent {
+    private _calculatorService = inject(CalculatorService);
     public calculatorButtons = viewChildren(CalculatorButtonComponent);
 
+    public resultText = computed(() => this._calculatorService.resultText());
+    public subResultText = computed(() => this._calculatorService.subResultText());
+    public lastOperator = computed(() => this._calculatorService.lastOperator());
+
     handleClick(key: string) {
-        console.log({key});
+        const keyValue = keyEquivalents[key] ?? key;
+        this._calculatorService.constructOperation(keyValue);
     }
 
     // @HostListener('document:keyup', ['$event']) – (ya no se recomienda, se cambia por la escritura en host)
     handleKeyboardEvent({key}: KeyboardEvent) {
-        const keyEquivalents: Record<string, string> = {
-            Escape: 'C',
-            Clear: 'C',
-            c: 'C',
-            '*': '⨉',
-            '/': '÷',
-            Enter: '='
-        }
-
         const keyValue = keyEquivalents[key] ?? key;
         this.handleClick(keyValue);
 
